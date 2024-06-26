4400b0e1ee13df6eaccf59a7ea95db43687e3263
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2024-06-26 14:44:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boxcodes
+
+A Flutter app for keeping track of what is in a storage box and where the storage box is located at home!
---
+++ android/app/src/main/kotlin/com/example/boxcodes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.boxcodes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
