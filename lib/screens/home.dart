2a5a3536105386120cde50a70619874cf816501b
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2024-07-10 14:46:10 -0400

    Add bottom navigation bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,23 +2,34 @@ import 'package:boxcodes/widgets/box_form.dart';
 import 'package:boxcodes/widgets/boxes_view.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BoxForm(),
-            BoxesView(),
-          ],
-        ),
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: const <Widget>[
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.add), label: 'Add')
+        ],
       ),
+      body: <Widget>[const BoxForm(), const BoxesView()][currentPageIndex],
     );
   }
 }
