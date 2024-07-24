2f746349feb3e7570cc5720b12c804f5387b11af
bdvuong <vuong.dbrandon@gmail.com>
2024-07-24 14:53:49 -0400

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,13 @@
+import 'package:boxcodes/widgets/boxes_view.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({
+  const HomePage({
     super.key,
   });
 
-  int currentPageIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    return const Text("Home Page");
+    return const Center(child: BoxesView());
   }
 }
